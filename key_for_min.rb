dfc4ee5bfab7f679e77a43e38ffd8fb567d96110
madiwa Simon <mad.simon01@gmail.com>
2020-11-17 17:14:24 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 require 'pry'
 def key_for_min_value(name_hash)
   name_value = 99999999
-  min_value = {" " => 99999999}
+  min_value = [ nil, 99999999]
   name_hash.each do |name, value|
-    if value < name_value && value < min_value
-      min_value = {name => value}
+    if value < name_value && value < min_value[1]
+      min_value = [name, value]
     end
+    min_value
   end 
-  binding.pry
-  min_value
+  min_value[0]
 end
