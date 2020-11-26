6d77189d54b94a13270267626fc76d832e1f7682
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-26 17:30:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,13 @@
 
 def key_for_min_value(name_hash)
   
-  min_key = nil
+  final_key = nil
   min_value = 1000
-  
-  name_hash.each do |key, value|
-  if min_value > value
-    min_value = value
-    min_key = key
-  end
+  name_hash.each do |key,value|
+    if min_value > value
+       min_value = value
+       final_key = key
+  end 
 end
-  p min_key
+p final_key
 end
